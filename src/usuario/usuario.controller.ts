e74118d5c8335509c02ef6b32c279937425302d9
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-04 13:14:28 -0300

    cap 1 finalizado

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,16 +1,12 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
 
 @Controller('users')
 export class UsuarioController {
-  private usuarios = [];
-
+  private usuarioService = new UsuarioService()
   @Post()
-  public cria(@Body() usuario) {
-    this.usuarios.push(usuario);
-    return usuario;
+  public cria(@Body() usuario) {  
+    return this.usuarioService.cria(usuario)
   }
-  @Get() 
-  public pegaTodos() {
-    return this.usuarios;
-  }
+
 }
