429675b8efb0021706661faa95d512e581ca4514
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-04 15:03:28 -0300

    cap 3 vid 1 finalizado

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,16 @@
+import { IsDate, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class Usuario {
+  @IsNotEmpty()
   id: number;
+  @IsString()
   nomeDeUsuario: string;
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
   senha: string;
+  @IsNotEmpty()
   nomeCompleto: string;
+  @IsDate()
   dataDeEntrada: Date;
 }
