ec9a70e51eb111044d72d40f8437f7d4d8d7fd11
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-05 15:52:50 -0300

    cap 5 vid 2 finalizado

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,21 +1,37 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { NestResponse } from 'src/core/http/nest-response';
+import { NestResponseBuilder } from 'src/core/http/nest-response-builder';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Controller('users')
 export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
-  
+
   @Get(':nomeDeUsuario')
-  public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string): Usuario {
-    return this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario)
+  public buscaPorNomeDeUsuario(
+    @Param('nomeDeUsuario') nomeDeUsuario: string,
+  ): Usuario {
+    return this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario);
   }
-  
+
   @Post()
-  public cria(@Body() usuario: Usuario, @Res() res) {
+  public cria(@Body() usuario: Usuario): NestResponse {
     const usuarioCriado = this.usuarioService.cria(usuario);
-    res.status(HttpStatus.CREATED)
-      .location(`/users/${usuarioCriado.nomeDeUsuario}`)
-      .json(usuario)
+    return new NestResponseBuilder()
+      .comStatus(HttpStatus.CREATED)
+      .comHeaders({
+        Location: `/users/${usuarioCriado.nomeDeUsuario}`,
+      })
+      .comBody(usuarioCriado)
+      .build();
   }
 }
