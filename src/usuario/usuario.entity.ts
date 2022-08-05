5b4977daeb87e4dc2fbe3936be90432bdebd74f9
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-05 13:29:48 -0300

    cap 4 vid 1 finalizado

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,35 +1,56 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IsDate, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico.validator';
 
 export class Usuario {
   id: number;
 
-  @IsNomeDeUsuarioUnico({
-    message: "nomeDeUsuario precisa ser único"
+  @Expose({
+    name: 'username'
   })
-
+  @IsNomeDeUsuarioUnico({
+    message: 'nomeDeUsuario precisa ser único',
+  })
   @IsNotEmpty({
-      message: 'nomeDeUsuario é obrigatório.'
+    message: 'nomeDeUsuario é obrigatório.',
   })
   @IsString({
-      message: 'nomeDeUsuario precisa ser uma string.'
+    message: 'nomeDeUsuario precisa ser uma string.',
   })
   nomeDeUsuario: string;
 
-  @IsEmail({}, {
-      message: 'email precisa ser um endereço de email válido.'
+  @Expose({
+    name: 'email'
   })
+  @IsEmail(
+    {},
+    {
+      message: 'email precisa ser um endereço de email válido.',
+    },
+  )
   email: string;
 
+  @Expose({
+    name: 'password'
+  })
+  @Exclude({
+    toPlainOnly: true
+  })
   @IsNotEmpty({
-      message: 'senha é obrigatório.'
+    message: 'senha é obrigatório.',
   })
   senha: string;
 
+  @Expose({
+    name: 'fullname'
+  })
   @IsNotEmpty({
-      message: 'nomeCompleto é obrigatório.'
+    message: 'nomeCompleto é obrigatório.',
   })
   nomeCompleto: string;
 
+  @Expose({
+    name: 'joinDate'
+  })
   dataDeEntrada: Date;
 }
